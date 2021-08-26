c38d802b3d9b97d0f93ce5b1d766de5ed671bb8a
kritikas726 <kritikas726@gmail.com>
2021-08-27 00:14:35 +0530

    added sign in page and signed in with facebook/google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyBih9eQcC1pfV1LA0Ze5WkdQWfQBat1rUI',
@@ -9,3 +11,5 @@ const config = {
   appId: '1:465295029634:web:49aceb8c06bf639f604b9b',
 };
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
